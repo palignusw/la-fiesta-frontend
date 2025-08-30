bc3e376a62af05d9a1a9d8029d39323e11b9c1a5
palignusw <firstnamelastname252003@gmail.com>
2025-08-30 17:44:28 +0400

    Update site URL in sitemap configuration
    Changed the site URL in sitemap.ts from 'https://lafiestamartvili.ge' to 'https://lafiesta.ge' to reflect the new domain.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from 'next'
-const site = 'https://lafiestamartvili.ge'
+const site = 'https://lafiesta.ge'
 
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
