fe5b578d18c95ced0eebe7927cbf80852562eee9
palignusw <firstnamelastname252003@gmail.com>
2025-09-12 04:05:08 +0400

    update layuot

--- app/layout.tsx
+++ app/layout.tsx
@@ -141,22 +141,27 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         {/* GA4 скрипты */}
-        {GA_ID && (
+        {
           <>
             <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              src="https://www.googletagmanager.com/gtag/js?id=AW-10987973823"
               strategy="afterInteractive"
             />
-            <Script id="ga4-init" strategy="afterInteractive">
+            <Script id="gtag-init" strategy="afterInteractive">
               {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}', { page_path: window.location.pathname });
-              `}
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-10987973823');
+    ${
+      GA_ID
+        ? `gtag('config', '${GA_ID}', { page_path: window.location.pathname });`
+        : ""
+    }
+  `}
             </Script>
           </>
-        )}
+        }
       </head>
       <body>
         <Suspense fallback={null}>
